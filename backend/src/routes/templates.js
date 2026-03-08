4cb5eb3459bb59ab3604739d369e78f8054df54a
npxpatel <nirajkumarpatel21@gmail.com>
2026-03-08 20:07:43 +0530

    Default route for templates

--- backend/src/routes/templates.js
+++ backend/src/routes/templates.js
@@ -1,5 +1,5 @@
 const { Router } = require('express');
-const { getTemplates, getTemplate, createTemplate, updateTemplate, deleteTemplate } = require('../controllers/templateController');
+const { getTemplates, getTemplate, createTemplate, updateTemplate, deleteTemplate, seedDefaultTemplates } = require('../controllers/templateController');
 const { authenticate } = require('../middleware/authenticate');
 
 const router = Router();
@@ -7,6 +7,7 @@ const router = Router();
 router.use(authenticate);
 
 router.get('/',                     getTemplates);
+router.post('/seed-defaults',       seedDefaultTemplates);
 router.get('/:templateId',          getTemplate);
 router.post('/',                    createTemplate);
 router.patch('/:templateId',        updateTemplate);
